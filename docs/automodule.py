b3bded1ee33414e20743ad453f8e98aaccbb51df
Loic Guilmard <loic.guilmard@cnrs.fr>
2025-01-14 10:57:10 +0100

    Added automodule generation

---
+++ docs/automodule.py
@@ -0,0 +1,12 @@
+import site
+from os import system
+
+pymodaq_module_list = ["pymodaq_utils", "pymodaq_gui", "pymodaq_data"]
+
+if __name__ == "__main__":
+
+    module_path = [path for path in site.getsitepackages() if path.endswith("site-packages")][0]
+
+    for module in pymodaq_module_list:
+        system(f"sphinx-apidoc.exe -e -o ./src/api/{module} {module_path}\\{module}")
+    
