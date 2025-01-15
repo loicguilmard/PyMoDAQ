2fe0037eab09c642084e3487adb2149228ec657b
Loic Guilmard <loic.guilmard@cnrs.fr>
2025-01-15 21:07:03 +0100

    typo

--- docs/automodule.py
+++ docs/automodule.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     module_path = [path for path in site.getsitepackages() if path.endswith("site-packages")][0]
 
     for module in pymodaq_module_list:
-        system(f"sphinx-apidoc -e -o ./src/api/{module} {module_path}\\{module}")
+        system(f"sphinx-apidoc -e -o ./src/api/{module} {module_path}/{module}")
     
